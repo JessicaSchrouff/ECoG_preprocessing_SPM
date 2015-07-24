918484dddd79285bb19ce7121f4eedd31f023300
JessicaSchrouff <jschrouff@ulg.ac.be>
2015-07-24 15:03:07 -0700

    info and delete not useful code

--- Anonymize_edf/edf_modify_channel_names.m
+++
@@ -1,8 +0,0 @@
-filename='C:\Users\Pierre Megevand\Documents\postdoc NSLIJ\lij048 StNy\CCEP\StNy_CCEP.edf';
-hdr=edf_hdr_reader(filename);
-lbl=StNy_chans_modPM; % electrode names in single-column text file imported with MATLAB Import Data Wizard
-for k=1:numel(lbl)
-    lbl{k}={lbl{k}};
-end
-hdr.Labels=lbl;
-edf_hdr_writer(filename,hdr,'mods');
